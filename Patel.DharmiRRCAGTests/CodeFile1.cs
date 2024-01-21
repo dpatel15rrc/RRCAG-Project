5aa5555997c75bb51e6473562ce0cf05f4ca7e1b
dpatel15rrc <129793193+dpatel15rrc@users.noreply.github.com>
2024-01-21 12:04:47 -0600

    Unit Testing for Financial class
    Contains unit tests for GetPayment() method from the Financial class.

---
+++ Patel.DharmiRRCAGTests/CodeFile1.cs
@@ -0,0 +1,128 @@
+﻿using Patel.Dharmi.Business;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patel.Dharmi.RRCAGTests
+{
+    class TestProgram
+    {
+        static void Main(string[] args)
+        {
+            TestSalesQuoteConstructor();
+            TestSetTradeInAmountMethod();
+            TestGetExteriorFinishCostMethod();
+            TestGetTotalMethod();
+
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Testing the SalesQuote(decimal, decimal, decimal) constructor.
+        /// </summary>
+        static void TestSalesQuoteConstructor()
+        {
+            Console.WriteLine("\nTesting SalesQuote(decimal, decimal, decimal) constructor. \n");
+
+            decimal vehicleSalePrice = 10000m,
+                    tradeInAmount = 5000m,
+                    salesTaxRate = 0.08m;
+
+            SalesQuote target = new SalesQuote(vehicleSalePrice, tradeInAmount, salesTaxRate);
+
+            decimal expectedVehicleSalePrice = 10000m,
+                    actualVehicleSalePrice = target.GetVehicleSalePrice();
+
+            decimal expectedTradeInAmount = 5000m,
+                    actualTradeInAmount = target.GetTradeInAmount();
+
+            decimal expectedSalesTax = 10000m * 0.08m,
+                    actualSalesTax = target.GetSalesTax();
+
+            Console.WriteLine("Test 1");
+
+            Console.WriteLine("Expected VehicleSalePrice : {0}\nActual VehicleSalePrice : {1}\n", expectedVehicleSalePrice, actualVehicleSalePrice);
+
+            Console.WriteLine("Expected TradeInAmount : {0}\nActual TradeInAmount : {1}\n", expectedTradeInAmount, actualTradeInAmount);
+
+            Console.WriteLine("Expected SalesTax : {0}\nActual SalesTax : {1}\n", expectedSalesTax, actualSalesTax);
+
+        }
+
+        /// <summary>
+        /// Testing SetTradeInAmount(decimal) : void method.
+        /// </summary>
+        static void TestSetTradeInAmountMethod()
+        {
+            Console.WriteLine("\nTesting SetTradeInAmount(decimal) : void method. \n");
+
+            decimal vehicleSalePrice = 10000m,
+                    tradeInAmount = 5000m,
+                    salesTaxRate = 0.08m;
+
+            SalesQuote target = new SalesQuote(vehicleSalePrice, tradeInAmount, salesTaxRate);
+            target.SetTradeInAmount(6000m);
+
+            decimal expectedTradeInAmount = 6000m,
+                    actualTradeInAmount = target.GetTradeInAmount();
+
+            Console.WriteLine("Test 1");
+
+            Console.WriteLine("Expected TradeInAmount : {0}\nActual TradeInAmount : {1}\n", expectedTradeInAmount, actualTradeInAmount);
+
+        }
+
+        /// <summary>
+        /// Testing GetExteriorFinishCost() : decimal method.
+        /// </summary>
+        static void TestGetExteriorFinishCostMethod()
+        {
+            Console.WriteLine("\nTesting GetExteriorFinishCost() : decimal method. \n");
+
+            decimal vehicleSalePrice = 10000m,
+                    tradeInAmount = 5000m,
+                    salesTaxRate = 0.08m;
+            Accessories accessoriesChosen = Accessories.None;
+            ExteriorFinish exteriorFinishChosen = ExteriorFinish.Standard;
+
+            SalesQuote target = new SalesQuote(vehicleSalePrice, tradeInAmount, salesTaxRate, accessoriesChosen, exteriorFinishChosen);
+
+            decimal expectedCost = 202.02m,
+                    actualCost = target.GetExteriorFinishCost();
+
+            Console.WriteLine("Test 1");
+
+            Console.WriteLine("Expected ExteriorFinishCost : {0}\nActual ExteriorFinishCost : {1}\n", expectedCost, actualCost);
+
+        }
+
+        /// <summary>
+        /// Testing GetTotal() : decimal method.
+        /// </summary>
+        static void TestGetTotalMethod()
+        {
+            Console.WriteLine("\nTesting GetTotal() : decimal method. \n");
+
+            decimal vehicleSalePrice = 10000m,
+                    tradeInAmount = 5000m,
+                    salesTaxRate = 0.08m;
+            Accessories accessoriesChosen = Accessories.All;
+            ExteriorFinish exteriorFinishChosen = ExteriorFinish.Standard;
+
+            SalesQuote target = new SalesQuote(vehicleSalePrice, tradeInAmount, salesTaxRate, accessoriesChosen, exteriorFinishChosen);
+            decimal expectedTotal = 14290.91m, //10000 + (505.05 + 1010.10 + 1515.15) + 202.02 + (10000 + (505.05 + 1010.10 + 1515.15) + 202.02) * 0.08
+                    actualTotal = target.GetTotal();
+
+            Console.WriteLine("Test 1");
+
+            Console.WriteLine("Expected TotalCost : {0}\nActual TotalCost : {1}\n", expectedTotal, actualTotal);
+
+        }
+
+    }
+
+}
---
+++ Patel.DharmiRRCAGTests/TestProgram.cs
@@ -0,0 +1,310 @@
+﻿/*
+ * Name: Dharmi Patel
+ * Program: Business Information Technology
+ * Course: ADEV-2008 Programming 2
+ * Created: 2023-09-18
+ * Updated: 
+ */
+
+using Patel.Dharmi.Business;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patel.Dharmi.RRCAGTests
+{
+    /// <summary>
+    /// Represents a test program to test GetPayment(decimal, int, decimal) method.
+    /// </summary>
+    class TestProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing GetPayment(decimal, int, decimal) method.");
+
+            Console.WriteLine("\n\nTest cases for ' decimal rate ' parameter: \n");
+            TestGetPayment_RateBelowZero_ExceptionThrown();
+            TestGetPayment_RateEqualsZero();
+            TestGetPayment_ValidRate();
+            TestGetPayment_RateEqualsOne();
+            TestGetPayment_RateAboveOne_ExceptionThrown();
+
+            Console.WriteLine("\n\nTest cases for ' int paymentPeriods ' parameter: ");
+            TestGetPayment_PmtPeriodsBelowZero_ExceptionThrown();
+            TestGetPayment_PmtPeriodsEqualsZero_ExceptionThrown();
+            TestGetPayment_ValidNumberOfPaymentPeriods();
+
+            Console.WriteLine("\n\nTest cases for ' decimal presentValue ' parameter: ");
+            TestGetPayment_PresentValueBelowZero_ExceptionThrown();
+            TestGetPayment_PresentValueEqualsZero_ExceptionThrown();
+            TestGetPayment_ValidPresentValue();
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Utility method to obtain only the message from an Exception object.
+        /// </summary>
+        /// <param name="exceptionMessage">The Exception's Message state.</param>
+        /// <returns>The Exception's message with the parameter omitted.</returns>
+        public static string GetExceptionMessage(string exceptionMessage)
+        {
+            return new System.IO.StringReader(exceptionMessage).ReadLine();
+        }
+
+        /// <summary>
+        /// Testing the Rate parameter when its value is less zero.
+        /// </summary>
+        static void TestGetPayment_RateBelowZero_ExceptionThrown()
+        {
+            Console.WriteLine("TEST CASE 1: When rate < 0.");
+            decimal result;
+            decimal rate = -0.05m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 12;
+
+            try
+            {
+                result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+                Console.WriteLine("Expected: The argument cannot be less than 0. Parameter name: rate.");
+                Console.WriteLine("Actual: " + result);
+            }
+
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine("Expected: The argument cannot be less than 0. Parameter name: rate");
+                Console.WriteLine("Actual: {0} Parameter name: {1}", GetExceptionMessage(exception.Message), (exception.ParamName) );
+            }
+        }
+
+        /// <summary>
+        /// Testing the Rate parameter when its value is equal to zero.
+        /// </summary>
+        static void TestGetPayment_RateEqualsZero()
+        {
+            Console.WriteLine("\nTEST CASE 2: When rate = 0.00 .");
+            decimal result;
+            decimal rate = 0.00m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 12;
+
+            decimal expected = 833.33m;
+            // 1000 / 12; 
+
+            result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+            Console.WriteLine("Expected: " + expected);
+            Console.WriteLine("Actual: " + result);
+        }
+
+        /// <summary>
+        /// Testing the Rate parameter when its value is more than 0 but less than 1.
+        /// </summary>
+        static void TestGetPayment_ValidRate()
+        {
+            Console.WriteLine("\nTEST CASE 3: When  0 < rate < 1.");
+            decimal result;
+            decimal rate = 0.05m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 12;
+
+            decimal expected = 1128.25m;
+            //0.05m * (0 + 10000m * (decimal)Math.Pow((double)(1 + 0.05m), (double)12)) / (((decimal)Math.Pow((double)(1 + 0.05m), (double)12) - 1) * (1 + 0.05m * 0));
+
+            result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+            Console.WriteLine("Expected: " + expected);
+            Console.WriteLine("Actual: " + result);
+        }
+
+        /// <summary>
+        /// Testing the Rate parameter when its value is more than 0 but less than 1.
+        /// </summary>
+        static void TestGetPayment_RateEqualsOne()
+        {
+            Console.WriteLine("\nTEST CASE 4: When rate = 1.");
+            decimal result;
+            decimal rate = 1.00m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 12;
+
+            decimal expected = 10002.44m;
+            //1.00m * (0 + 10000m * (decimal)Math.Pow((double)(1 + 1.00m), (double)12)) / (((decimal)Math.Pow((double)(1 + 1.00m), (double)12) - 1) * (1 + 1.00m * 0));
+
+            result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+            Console.WriteLine("Expected: " + expected);
+            Console.WriteLine("Actual: " + result);
+        }
+
+        /// <summary>
+        /// Testing the Rate parameter when its value is more than 1.
+        /// </summary>
+        static void TestGetPayment_RateAboveOne_ExceptionThrown()
+        {
+            Console.WriteLine("\nTEST CASE 5: When rate > 1.");
+            decimal result;
+            decimal rate = 1.5m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 12;
+
+            try
+            {
+                result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+                Console.WriteLine("Expected: The argument cannot be less than 0. Parameter name: rate.");
+                Console.WriteLine("Actual: " + result);
+            }
+
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine("Expected: The argument cannot be greater than 1. Parameter name: rate");
+                Console.WriteLine("Actual: {0} Parameter name: {1}", GetExceptionMessage(exception.Message), (exception.ParamName) );
+            }
+
+        }
+
+        /// <summary>
+        /// Testing the numberOfPaymentPeriods parameter when its value is less than zero.
+        /// </summary>
+        static void TestGetPayment_PmtPeriodsBelowZero_ExceptionThrown()
+        {
+            Console.WriteLine("\nTEST CASE 6: When numberOfPaymentPeriods < 0.");
+            decimal result;
+            decimal rate = 0.05m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = -1;
+
+            try
+            {
+                result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: numberOfPaymentPeriods");
+                Console.WriteLine("Actual: " + result);
+            }
+
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: numberOfPaymentPeriods");
+                Console.WriteLine("Actual: {0} Parameter name: {1}", GetExceptionMessage(exception.Message), (exception.ParamName));
+            }
+
+        }
+
+        /// <summary>
+        /// Testing the numberOfPaymentPeriods parameter when its value is equal to zero.
+        /// </summary>
+        static void TestGetPayment_PmtPeriodsEqualsZero_ExceptionThrown()
+        {
+            Console.WriteLine("\nTEST CASE 7: When numberOfPaymentPeriods = 0.");
+            decimal result;
+            decimal rate = 0.05m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 0;
+
+            try
+            {
+                result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: numberOfPaymentPeriods");
+                Console.WriteLine("Actual: " + result);
+            }
+
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: numberOfPaymentPeriods");
+                Console.WriteLine("Actual: {0} Parameter name: {1}", GetExceptionMessage(exception.Message), (exception.ParamName));
+            }
+
+        }
+
+        /// <summary>
+        /// Testing the numberOfPaymentPeriods parameter when its value is more than zero.
+        /// </summary>
+        static void TestGetPayment_ValidNumberOfPaymentPeriods()
+        {
+            Console.WriteLine("\nTEST CASE 8: When numberOfPaymentPeriods > 0.");
+            decimal result;
+            decimal rate = 0.05m,
+                    presentValue = 10000m;
+            int numberOfPaymentPeriods = 12;
+
+            decimal expected = 1128.25m;
+            //0.05m * (0 + 10000m * (decimal)Math.Pow((double)(1 + 0.05m), (double)12)) / (((decimal)Math.Pow((double)(1 + 0.05m), (double)12) - 1) * (1 + 0.05m * 0));
+
+            result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+            Console.WriteLine("Expected: " + expected);
+            Console.WriteLine("Actual: " + result);
+        }
+
+        /// <summary>
+        /// Testing the presentValue parameter when its value is below zero.
+        /// </summary>
+        static void TestGetPayment_PresentValueBelowZero_ExceptionThrown()
+        {
+            Console.WriteLine("\nTEST CASE 9: When presentValue < 0.");
+            decimal result;
+            decimal rate = 0.05m,
+                    presentValue = -10000m;
+            int numberOfPaymentPeriods = 12;
+
+            try
+            {
+                result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: presentValue");
+                Console.WriteLine("Actual: " + result);
+            }
+
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: presentValue");
+                Console.WriteLine("Actual: {0} Parameter name: {1}", GetExceptionMessage(exception.Message), (exception.ParamName));
+            }
+
+        }
+
+        /// <summary>
+        /// Testing the presentValue parameter when its value is equal to zero.
+        /// </summary>
+        static void TestGetPayment_PresentValueEqualsZero_ExceptionThrown()
+        {
+            Console.WriteLine("\nTEST CASE 10: When presentValue = 0.");
+            decimal result;
+            decimal rate = 0.05m,
+                    presentValue = 0m;
+            int numberOfPaymentPeriods = 12;
+
+            try
+            {
+                result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: presentValue");
+                Console.WriteLine("Actual: " + result);
+            }
+
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine("Expected: The argument cannot be less than or equal to 0. Parameter name: presentValue");
+                Console.WriteLine("Actual: {0} Parameter name: {1}", GetExceptionMessage(exception.Message), (exception.ParamName));
+            }
+
+        }
+
+        /// <summary>
+        /// Testing the presentValue parameter when its value is more than zero.
+        /// </summary>
+        static void TestGetPayment_ValidPresentValue()
+        {
+            Console.WriteLine("\nTEST CASE 11: When presentValue > 0.");
+            decimal result;
+            decimal rate = 0.0575m,
+                    presentValue = 19241.41m;
+            int numberOfPaymentPeriods = 12;
+
+            decimal expected = 1128.25m;
+            //0.05m * (0 + 10000m * (decimal)Math.Pow((double)(1 + 0.05m), (double)12)) / (((decimal)Math.Pow((double)(1 + 0.05m), (double)12) - 1) * (1 + 0.05m * 0));
+
+            result = Financial.GetPayment(rate, numberOfPaymentPeriods, presentValue);
+            Console.WriteLine("Expected: " + expected);
+            Console.WriteLine("Actual: " + result);
+        }
+
+    }
+
+}
